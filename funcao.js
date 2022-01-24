fbf1cf8d8dc91babed67417c8fe3f216fc2ab97d
beatrizanastacios@gmail.com <beatrizanastacios@gmail.com>
2022-01-24 17:45:10 -0300

    Implementação da barra de navegação e cabeçalho

---
+++ funcao.js
@@ -0,0 +1,12 @@
+
+// Função que altera o texto principal. 
+function mover(obj){
+    obj.innerHTML = "Olá, Mundo!"
+}
+
+function sair(obj){
+    obj.innerHTML = "Hello, World!"
+} // fim da função que altera o texto principal. 
+
+// Recado inicial sobre o status do site
+window.alert("Olá, o site está em desenvolvimento :)");
